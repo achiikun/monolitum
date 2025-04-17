1316470c4900e1b2115d90e9b705af499de71bd8
achi <1425109@uab.cat>
2025-04-17 09:14:50 +0200

    feat: create ListUtils.php

---
+++ monolitum/core/util/ListUtils.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace core\util;
+
+class ListUtils
+{
+
+    /**
+     * @param array $array
+     * @param mixed $element
+     * @param int|null $idx
+     * @return void
+     */
+    public static function insertAnElementIntoAnArray(&$array, $element, $idx = null)
+    {
+        if ($idx !== null) {
+            if ($array === null) {
+                $array = $element;
+            } else {
+                if (!is_array($array))
+                    $array = [$array];
+                array_splice($array, $idx, 0, [$element]);
+            }
+        } else {
+            if ($array === null) {
+                $array = $element;
+            } else if (!is_array($array)) {
+                $array = [$array, $element];
+            } else {
+                $array[] = $element;
+            }
+        }
+    }
+}
